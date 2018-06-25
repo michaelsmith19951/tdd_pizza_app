3fa09a7c9587bb0bad8c2933802eeef4623331d3
Michael Smith <michaelsmith19951@yahoo.com>
2018-06-25 11:43:51 -0400

    4th test that failed

--- tdd_pizza_app.rb
+++ tdd_pizza_app.rb
@@ -1,5 +1,8 @@
 # The available crusts
-def toppings()
+def crust_toppings()
 	crusts = ["stuffed crust", "thin crust", "flatbread", "thick crust"]
 end
 
+#The available meat toppings
+def meat_toppings()
+end
--- test_tdd_pizza_app.rb
+++ test_tdd_pizza_app.rb
@@ -7,9 +7,10 @@ require_relative "tdd_pizza_app.rb"
 class TestTddPizzaApp < Minitest::Test
 
 	def test_if_array_is_a_class
-		assert_equal(Array, toppings().class)
+		assert_equal(Array, crust_toppings().class)
 	end
 
-	
-
+	def test_if_second_array_is_a_class
+		assert_equal(Array, meat_toppings().class)
+	end
 end
